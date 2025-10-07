a6c688c38e7df655cde67fdd7f1e0b0b8f66bf73
Max96312 <k0mp4ny16@gmail.com>
2025-10-07 14:36:54 +0900

    sec06: 논리적 모델링2 - 참여도와 일대다 관계

--- DB/db-design1/sec06/34.sql
+++ DB/db-design1/sec06/34.sql
@@ -19,3 +19,16 @@ CREATE TABLE member (
 INSERT INTO member(name, team_id) VALUES ('이기획', NULL);
 
 SELECT * FROM member;
+
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member (
+    member_id   BIGINT       NOT NULL AUTO_INCREMENT,
+    team_id     BIGINT       NOT NULL, -- NOT NULL로 필수적 참여 구현
+    name        VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (member_id),
+    CONSTRAINT fk_member_team FOREIGN KEY (team_id)
+        REFERENCES team (team_id)
+);
+
+INSERT INTO member(name, team_id) VALUES ('이기획', NULL);
